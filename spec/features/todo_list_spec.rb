3c2c68336eff5720bd887420947545b24eea054c
Tacuma Bell <takumab@gmail.com>
2018-03-10 15:19:13 +0000

    Add todo list feature

--- spec/features/todo_list_spec.rb
+++ spec/features/todo_list_spec.rb
@@ -6,5 +6,14 @@ RSpec.describe "TodoList", :type => :feature do
         @todo_list = FactoryBot.create(:todo_list)
         TodoList.make(title: @todo_list.title)
       end
+
+      scenario "Create a todo list" do
+        visit '/todo_list/new'
+        within("form") do
+          fill_in 'Title', with: 'My First Todo List'
+        end
+        click_button 'Create Todo List'
+        expect(page).to have_content 'Todo List Created'
+      end
   end
 end
