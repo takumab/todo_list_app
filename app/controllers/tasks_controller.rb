70218ff4076805879c0da656d3142edf9fd2b2b4
Tacuma Bell <takumab@gmail.com>
2018-03-12 23:42:38 +0000

    Add some styling and added @todo_list to set_task because w/o it @todo_list is nil

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,6 @@ class TasksController < ApplicationController
     @todo_list = TodoList.find(params[:todo_list_id])
     @task = @todo_list.tasks.new(task_params)
     if @task.save
-      logger.debug "Task is saved"
       flash[:notice] = "Task created!"
       redirect_to @todo_list
     else
@@ -21,15 +20,15 @@ class TasksController < ApplicationController
   end
 
   def completed
-    @task.update_attributes(:completed, true)
+    @task.update_attribute(:completed, true)
     redirect_to root_path, notice: "Task completed!"
   end
 
   private
 
   def set_task
+    @todo_list = TodoList.find(params[:todo_list_id])
     @task = @todo_list.tasks.find(params[:id])
-
   end
 
   def set_todo_list
